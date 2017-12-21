dc844b330622bc0c1478610d167e0ab9e8da0e07
Sierra Moore <sierra.v.moore@gmail.com>
2017-12-21 16:11:31 -0600

    game board layout done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 console.log("tick tak toe")
 
+// give button clear board an on click event handler
 $('#clear-board').on('click', () =>{
     console.log("clicked")
 });
+//store board in variable to use later
+let board = $('#board');
+console.log(board);
+
+for(i=0; i < 9; i++){
+    //div are the squares
+    // make new div's with class square
+    let div = $('<div>').attr('class', 'square');
+    //append div to board
+    $(board).append(div);
+
+}
