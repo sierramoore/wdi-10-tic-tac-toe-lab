30a56425346f16b16b1025e919f61116b1280043
Sierra Moore <sierra.v.moore@gmail.com>
2017-12-21 16:23:25 -0600

    made a square event handler for game board

--- js/app.js
+++ js/app.js
@@ -9,10 +9,14 @@ let board = $('#board');
 console.log(board);
 
 for(i=0; i < 9; i++){
-    //div are the squares
-    // make new div's with class square
+    // make new div's with class square -> will be used as gameboard
     let div = $('<div>').attr('class', 'square');
     //append div to board
     $(board).append(div);
-
 }
+
+//naming someFunction will create later as a placeholder
+$('.square').on('click', someFunction = () =>{
+    console.log("square clicked");
+});
+
