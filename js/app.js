8110b54fa9f81d3418c9d8d231d067da9ae9ebf3
Sierra Moore <sierra.v.moore@gmail.com>
2017-12-24 21:48:37 -0600

    in progress of creating logic

--- js/app.js
+++ js/app.js
@@ -1,51 +1,127 @@
 console.log("tick tak toe")
 
+
+
 // give button clear board an on click event handler
 $('#clear-board').on('click', () =>{
-    //not good way to clear fix later
-    board.children().text("");
-    console.log("clicked")
+    console.log("clicked");
+    $('.square').html("");
 });
-//store board in variable to use later
-let board = $('#board');
 
-// console.log(board);
 
-//global variable for x and o toggle
-let xo = true;
+let isGameOver = function (state, coordinates){
+  return true;
+};
+
+let gameOver = function (){
+    console.log("game over")
+};
+
 
 // event handler for event listener in for loop
 function someFunction(event) {
-    // console.log(event);
-    // console.log("this function someFunction was run");
 
-    if(xo === true){
-        xo = false;
-        $(event.currentTarget).html("X").off("click");
-        $('#turn').html("Now its O's turn")
+    //gets cuurent click and get parnt element which is the entire board
+    //and after we get board then we get the custom attr
+    //using current tartget we can acseess the coordinates of the current div
+    let state = $(event.currentTarget).parent().data("state");
+    console.log(state);
+    let coordinates = $(event.currentTarget).data("coordinates");
+    console.log(coordinates);
 
+
+    // if (the cell is not empty)
+    if(state.cells[coordinates.row][coordinates.col] !== " ")
+        return;
+
+
+    if(state.xo === true){
+        state.xo = false;
+        $(event.currentTarget).html("X");
+        //replace empty space in array with occupied x
+        state.cells[coordinates.row][coordinates.col] = "X";
+        $('#turn').html("Now its O's turn");
     }else{
-        xo = true;
-        $(event.currentTarget).html("O").off("click");
-        $('#turn').html("Now its X's turn")
+        state.xo = true;
+        $(event.currentTarget).html("O");
+        //replace empty space in array with occupied O
+        state.cells[coordinates.row][coordinates.col] = "O";
+        $('#turn').html("Now its X's turn");
 
     }
 
+    if (isGameOver(state, coordinates))
+        gameOver();
 }
 
+let createBoard = function(cols, rows){
+    let board = $('<div>');
+
+    let state =  {
+        xo: true,
+        rows: rows,
+        cols: cols,
+        //saves state of board and checks for win after each move
+        //parent array
+        cells: new Array(rows)
+
+    };
+    //string "state" is the custom attr -- state is the obj
+    board.data("state", state);
+    console.log(board);
+
+    for(i=0; i < rows; i++){
+        //creates an array to hold the state of the elements of the current row
+        //acseess current row in parent array and assign it the current iteration of row
+        //initialize child array with number of colums
+        state.cells[i] = new Array(cols);
+
+        for(j=0; j < cols; j++){
+            let cell = $('<div>').attr('class', 'square');
+            cell.data("coordinates", {row: i, col: j});
+            cell.on('click', someFunction);
+            $(board).append(cell);
+
+            //for each cols make an empty space string to indicate an unplayed cell
+            state.cells[i][j] = " ";
+        }
+        //after each row insert break
+        let endRow = $('<br>');
+        $(board).append(endRow);
+    }
+
+    return board;
+};
+let b = createBoard(3, 3);
+//append board to the body
+$('body').append(b);
 
 
-for(i=0; i < 9; i++){
-    // make new div's with class square -> will be used as gameboard
-    let div = $('<div>').attr('class', 'square');
-    //append div to board
-    $(board).append(div);
+$('<br>').appendTo('body');
+$('body').append(createBoard(5, 3));
 
 
-    //created event listener using event handler as callback -> applies to individual div
-    div.on('click', someFunction);
-// someFunction(e);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
